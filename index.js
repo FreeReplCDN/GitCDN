6f1136468a7ad1aae6e486800cf59c4d2b01f1fb
py660 <93660666+py660@users.noreply.github.com>
2024-06-17 23:04:21 -0400

    Cleanup + Update template

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import { getUserInfo } from "@replit/repl-auth";
 import { listFiles, createRepo } from "./git.mjs";
 import { sql } from "./sql.mjs";
 import * as fs from "fs";
@@ -12,21 +11,6 @@ import { generateAuth } from "./auth.mjs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Write uptime log file
-setInterval(() => {
-  let time = "\n" + new Date().toLocaleString("en-US");
-  fs.appendFile("other/pinger.log", time, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(time);
-      console.log("File written successfully\n");
-      //console.log("The written has the following contents:");
-      //console.log(fs.readFileSync("books.txt", "utf8"));
-    }
-  });
-}, 120000);
-
 const app = express();
 
 app.use(cookieParser());
@@ -34,18 +18,18 @@ app.use(express.static(path.join(__dirname, "/public/")));
 
 // Homepage
 app.get("/", async (req, res) => {
-  const user = getUserInfo(req);
   res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+app.get("/main2", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index2.html"));
+});
+app.get("/template", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/template.html"));
 });
 
 // Login page
 app.get("/login", async (req, res) => {
-  const user = getUserInfo(req);
-  if (!user) {
     res.sendFile(path.join(__dirname, "/views/login.html"));
-  } else {
-    res.redirect("/dashboard");
-  }
 
   /* Implementing repl Auth */
   /* reinventing auth */
@@ -53,31 +37,12 @@ app.get("/login", async (req, res) => {
 
 // View your files
 app.get("/dashboard", async (req, res) => {
-  const user = getUserInfo(req);
   //don't use replit auth for all endpoints, but instead use a session cookie? maybe because of github auth standardization. Yes, you're right
   /* If user logged in we go to the dashboard. */
-  if (user) {
     /* TODO view in DB if user is already registered. */
     res.sendFile(path.join(__dirname, "/views/dashboard.html"));
-  } else {
-    res.redirect("/login#error");
-  }
 });
 
-// Returns `pong` for UptimeRobot
-app.get("/ping", async (req, res) => {
-  let time = new Date().toLocaleString("en-US");
-  fs.appendFile("other/pinger.log", "\nPING RECV: " + time, (err) => {
-    if (err) console.log(err);
-    else {
-      console.log(time);
-      console.log("File written successfully for net request to ping\n");
-      //console.log("The written has the following contents:");
-      //console.log(fs.readFileSync("books.txt", "utf8"));
-    }
-  });
-  res.send("pong");
-});
 
 app.get("/services/oembed/", async (req, res) => {
   res.sendFile(path.join(__dirname, "other/oembed.json"));
--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,19 @@
-// set up text to print, each item in array is new line
-let text = [
-  "share your files",
-  "distribute your assets",
-  "host your code"
-];
-let destination = document.getElementById("heading");
-let textPos = 0;
-let row = 0;
- 
-function typewriterOld(reset)
-{
-  content =  `The fastest way to ${text[row]}`;
-  destination.innerHTML = content;
- 
- destination.innerHTML = sContents + aText[iIndex].substring(0, textPos) + "<span style='background-color:var(--accent-color); width:10px;'></span>";
- if ( iTextPos++ == iArrLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != aText.length ) {
-   iArrLength = aText[iIndex].length;
-   setTimeout(()=>{
-    iIndex = 0;
-    iTextPos = 0; // initialise text position
-    sContents = ''; // initialise contents variable
-    iRow = null; // initialise current row
-    destination.innerHTML = "";
-    typewriter();console.log(destination); typewriter()
-   }, 6000);
-  }
- } else {
-  setTimeout("typewriter()", iSpeed);
- }
-}
+// Effortlessly distribute your assets
+// Insert images into HTML with ease
+// Seamlessly share large files over email
 
-
-function typewriter(){
-  content = `The fastest way to ${text[row%text.length]}`;
-}
-
-setTimeout(()=>{
-  typewriter();
-}, 500);
+new TypeIt("#type", {
+  speed: 40,
+  loop: true
+}).pause(300)
+  .type("effortly distribute your assets")
+  .pause(1700)
+  .delete()
+  .pause(300)
+  .type("exchange files for URLs with ease")
+  .pause(1700)
+  .delete()
+  .pause(300)
+  .type("seamlessly transfer large files across platforms")
+  .pause(1700)
+  .go();
